7b757c19906e1dabcb8736667922896988631030
ppedvLukasK <lukask@ppedv.de>
2022-04-20 10:23:14 +0200

    Update Solid1.cs

--- Solid1.cs
+++ Solid1.cs
@@ -11,13 +11,11 @@ namespace Übung_Solid1
             IReportGenerator list10Generator = new List10();
 
             ReportGeneratorBase crystalReportGenerator = new CrystalReports();
-
         }
     }
 
 
     #region Bad Code
-
     public class BadEmployee
     {
         public int Employee_Id { get; set; }
@@ -53,121 +51,5 @@ namespace Übung_Solid1
             }
         }
     }
-
     #endregion
-
-
-    public interface IEmployee
-    {
-        string Marke { get; set; }
-        string Modell { get; set; }
-        int Baujahr { get; set; }
-    }
-
-    public class Employee : IEmployee
-    {
-        public string Marke { get; set; }
-        public string Modell { get; set; }
-        public int Baujahr { get; set; }
-    }
-
-    //Variante 1
-    public class EmployeeRepository
-    {
-        public void Insert(Employee employee)
-        {
-            //speicher einen Datensatz
-        }
-    }
-
-
-    #region Open Part
-    //Variante 1
-
-    public interface IReportGenerator
-    {
-        //C# 9.0 auf .NET 5
-        void Init ()
-        {
-            Console.WriteLine("Meine Basis Implementierung");
-        }
-
-        void Generate(IEmployee em);
-    }
-
-    //Variante 2
-    public abstract class ReportGeneratorBase
-    {
-        public string OutPutDirectory { get; set; }
-        void Init()
-        {
-            Console.WriteLine("Meine Basis Implementierung");
-        }
-
-        public virtual string ShowReportVersion()
-        {
-           return "Virtual Methode";
-        }
-        public abstract void Generate(IEmployee em);
-    }
-
-    //Variante 1 und 2 in Kombination
-    public abstract class ReportGeneratorBase2 : IReportGenerator
-    {
-        public abstract void Generate(IEmployee em);
-    }
-    #endregion
-
-    #region Close Part
-    //Variante 1 mit Interface
-    public class PDFReportGenerator : IReportGenerator
-    {
-        public void Generate(IEmployee em)
-        {
-            //Erstelle ein Report
-        }
-    }
-
-    //Variante 2 mit abstract class
-
-    public class CrystalReports : ReportGeneratorBase
-    {
-        public override void Generate(IEmployee em)
-        {
-            //Erstelle ein Report
-        }
-    }
-
-
-    public class List10 : ReportGeneratorBase2 
-    {
-        public override void Generate(IEmployee em)
-        {
-           //Erstelle ein Report
-        }
-    }
-    #endregion
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
