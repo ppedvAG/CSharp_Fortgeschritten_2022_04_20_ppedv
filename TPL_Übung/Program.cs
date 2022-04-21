ef589e30639fbd6fb0dc21105790df6356959d55
ppedvLukasK <lukask@ppedv.de>
2022-04-21 16:11:49 +0200

    TPL Übungen

---
+++ TPL_Übung/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+	static void Main(string[] args)
+	{
+        int until = 10000;
+        Task<List<long>> numbers = Task.Run(() => GetPrimeNumbers(10000));
+        List<long> result = numbers.Result;
+		Console.WriteLine($"Nummern berechnet bis {until}");
+
+        numbers.ContinueWith(_ => WriteNumbersToFile(result));
+		Console.WriteLine("Nummern geschrieben");
+        Console.ReadKey();
+    }
+
+    public static void WriteNumbersToFile(List<long> num)
+	{
+        File.WriteAllLines("file.txt", num.Select(e => e.ToString()).ToArray());
+	}
+
+    public static List<long> GetPrimeNumbers(long until)
+    {
+        List<long> retValues = new List<long>();
+        for (long i = 1; i < until; i += 2)
+            if (CheckPrime(i))
+                retValues.Add(i);
+        return retValues;
+    }
+
+    private static bool CheckPrime(long num)
+    {
+        for (long i = 2; i < num; i++)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+}
---
+++ TPL_Übung/Program2.cs
@@ -0,0 +1,50 @@
+﻿class Program2
+{
+    #region Solution
+    static void Main2(string[] args)
+    {
+        /* TPL Übung mit CancelationTokenSource
+        * In dieser Aufgabe soll die Methode ShowPrimeNumbers eine Möglichkeit bekommen, dass man 
+        * den Primzahlen Algorithmus von aussen beenden kann. 
+        * 
+        * Der Primzahlalgorithmus soll nach 20 Sekunden automatisch von der Main-Methode beendet werden 
+        */
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        Task easyTask = new Task(ShowPrimeNumbers, cts.Token);
+        easyTask.Start();
+
+        Thread.Sleep(20000);
+        cts.Cancel();
+
+        Console.WriteLine("20 Sekunden sind vorbei");
+        Console.ReadLine();
+    }
+
+    public static void ShowPrimeNumbers(object param)
+    {
+        CancellationToken cancellationToken = (CancellationToken) param;
+        long i = 0;
+
+        while (true)
+        {
+            i++;
+            if (chkprime(i))
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            if (cancellationToken.IsCancellationRequested)
+                cancellationToken.ThrowIfCancellationRequested();
+        }
+    }
+
+    private static bool chkprime(long num)
+    {
+        for (long i = 2; i < num; i++)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+    #endregion Solution
+}
